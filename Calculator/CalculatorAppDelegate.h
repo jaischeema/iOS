4ba74f1c348773fa96ddf24fbe434a9abdaaaf34
jaischeema <jaischeema@gmail.com>
2012-06-05 08:54:01 +1000

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Jais Cheema on 5/06/12.
+//  Copyright (c) 2012 CyberSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Jais Cheema on 5/06/12.
+//  Copyright (c) 2012 CyberSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
