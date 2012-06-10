b4c442ed69c4f6d922ef3db308cabc5dcd88589c
jaischeema <jaischeema@gmail.com>
2012-06-10 16:17:05 +1000

    Add the pinch gesture to the face view

--- Happiness/Happiness/HappinessViewController.m
+++ Happiness/Happiness/HappinessViewController.m
@@ -20,12 +20,24 @@
 @synthesize happiness = _happiness;
 @synthesize faceView = _faceView;
 
+-(void) setFaceView:(FaceView *)faceView
+{
+    _faceView = faceView;
+    UIPinchGestureRecognizer *gesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self.faceView action:@selector(pinch:)];
+    [self.faceView addGestureRecognizer:gesture];
+}
+
 - (void) setHappiness:(int)happiness
 { 
     if(happiness > 100) happiness = 100;
     if(happiness < 0 ) happiness = 0;
     _happiness = happiness;
     [self.faceView setNeedsDisplay];
+}
+
+- (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+{
+    return YES;
 }
 
 @end
