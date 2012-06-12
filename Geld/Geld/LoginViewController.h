9bd70821ed74b60bdd6f2829a406cbfbc0e35952
jaischeema <jaischeema@gmail.com>
2012-06-12 13:20:21 +1000

    Add login items

---
+++ Geld/Geld/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Geld
+//
+//  Created by Jais Cheema on 12/06/12.
+//  Copyright (c) 2012 CyberSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
